6edf182e1921f8f6608c2141b6aac31b5f39d052
Mohammad Ghamari <mohammadghamary76@outlook.com>
2025-12-11 02:56:54 +0330

    create on Document closed event

--- android/src/main/java/com/foxit/flutterfoxitpdf/FlutterFoxitpdfPlugin.java
+++ android/src/main/java/com/foxit/flutterfoxitpdf/FlutterFoxitpdfPlugin.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.WindowManager;
 
 import java.util.HashMap;
 import org.json.JSONObject;
