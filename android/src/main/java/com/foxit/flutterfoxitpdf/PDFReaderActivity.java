8f4ade8ddeb6e60631f875992a74c689907b2ffe
Mohammad Ghamari <mohammadghamary76@outlook.com>
2026-08-14 19:02:28 +0330

    improve security

--- android/src/main/java/com/foxit/flutterfoxitpdf/PDFReaderActivity.java
+++ android/src/main/java/com/foxit/flutterfoxitpdf/PDFReaderActivity.java
@@ -175,13 +175,13 @@ public class PDFReaderActivity extends FragmentActivity {
         Log.d(TAG, "decryptedCategory: " + decryptedCategory);
         Log.d(TAG, "bookCategory: " + bookCategory);
 
-        Log.d(TAG, "finalPassword: " + obfuscator.deobfuscate(obfuscatedBookName));
+        Log.d(TAG, "finalPassword: " + obfuscator.deobfuscate(decryptedCategory));
 
         int type = bundle.getInt("type", 0);
         if (type == 0) {
-            uiextensionsManager.openDocument(path, obfuscator.deobfuscate(obfuscatedBookName));
+            uiextensionsManager.openDocument(path, obfuscator.deobfuscate(decryptedCategory));
         } else {
-            pdfViewCtrl.openDocFromUrl(path, obfuscator.deobfuscate(obfuscatedBookName), null, null);
+            pdfViewCtrl.openDocFromUrl(path, obfuscator.deobfuscate(decryptedCategory), null, null);
         }
     }
 
